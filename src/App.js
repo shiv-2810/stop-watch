1ac5857c7d7d149681e538b3fdff3d5afd172c7d
Shivam Kumar <67449261+shiv-2810@users.noreply.github.com>
2023-11-30 22:32:17 +0530

    Stop watch basic functionality done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [checkRunning, setCheckRunning] = useState(false)
+  const[time, setTime] = useState(0)
+  const[hours, setHours] = useState(0)
+  const[minutes, setMinutes] = useState(0)
+  const[seconds, setSeconds] = useState(0)
+  const[milliSecs, setMilliSecs] = useState(0)
+
+  useEffect(()=>{
+    let intervalId;
+    if(checkRunning){
+      intervalId= setInterval(()=>setTime(time+1),10)
+    }
+    setHours(Math.floor(time / 360000))
+    setMinutes(Math.floor((time % 360000) / 6000))
+    setSeconds(Math.floor((time % 6000) / 100))
+    setMilliSecs(time % 100)
+    return () => clearInterval(intervalId);
+   
+  },[checkRunning, time])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{color:'black',marginBottom:10}}>Stop watch</div>
+        <div className='stop-box' style={{padding:30, display:'flex'}}>
+        {hours}:{minutes}:{seconds}:{milliSecs}
+        </div>
+        <div style={{display:'flex',gap:'20px',marginTop:'20px'}}>
+       <div className='start-btn' style={{cursor:'pointer'}} onClick={()=>setCheckRunning(!checkRunning)}>{`${checkRunning ? 'Pause' : 'Start'}`}</div>
+       <div className='reset-btn' style={{cursor:'pointer'}} onClick={()=>{setCheckRunning(false); setTime(0)}}>Reset</div>
+       </div>
       </header>
     </div>
   );
